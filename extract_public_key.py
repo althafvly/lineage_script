49b36bb22adc0b50df79536d658726460e58c445
althafvly <althafvly@gmail.com>
2025-07-31 23:50:15 +0530

    script: Update cert generation script

---
+++ extract_public_key.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import struct
+import argparse
+import getpass
+
+class AvbError(Exception):
+    pass
+
+def egcd(a, b):
+    """Extended GCD algorithm."""
+    if a == 0:
+        return b, 0, 1
+    gcd, x1, y1 = egcd(b % a, a)
+    return gcd, y1 - (b // a) * x1, x1
+
+def modinv(a, m):
+    """Modular inverse using extended GCD."""
+    gcd, x, _ = egcd(a, m)
+    if gcd != 1:
+        return None  # Modular inverse does not exist
+    return x % m
+
+def round_to_pow2(number):
+    """Round up to the next power of 2."""
+    return 2 ** ((number - 1).bit_length())
+
+def encode_long(num_bits, value):
+    """Encode an integer into a big-endian byte array."""
+    ret = bytearray()
+    for bit_pos in range(num_bits, 0, -8):
+        octet = (value >> (bit_pos - 8)) & 0xff
+        ret.extend(struct.pack('!B', octet))
+    return ret
+
+class RSAPublicKey:
+    MODULUS_PREFIX = b'modulus='
+
+    def __init__(self, key_path, password=None):
+        self.key_path = key_path
+        self.key_password = password
+
+        self.modulus = self._extract_modulus()
+        self.num_bits = round_to_pow2(self.modulus.bit_length())
+        self.exponent = 65537
+
+    def _extract_modulus(self):
+        """Extract the RSA modulus using openssl."""
+        base_args = ['openssl', 'rsa', '-in', self.key_path, '-modulus', '-noout']
+        if self.key_password:
+            base_args += ['-passin', f'pass:{self.key_password}']
+
+        for args in [base_args, base_args + ['-pubin']]:
+            with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+                stdout, stderr = proc.communicate()
+                if proc.returncode == 0:
+                    if stdout.lower().startswith(self.MODULUS_PREFIX):
+                        modulus_hex = stdout[len(self.MODULUS_PREFIX):].strip()
+                        return int(modulus_hex, 16)
+        raise AvbError(f"Failed to extract modulus:\n{stderr.decode()}")
+
+    def encode(self):
+        """Encode the public key for AVB (Android Verified Boot)."""
+        if self.exponent != 65537:
+            raise AvbError("Only exponent 65537 supported")
+
+        n0inv = (2 ** 32 - modinv(self.modulus % 2 ** 32, 2 ** 32)) % 2 ** 32
+        r = 2 ** self.num_bits
+        rrmodn = (r * r) % self.modulus
+
+        out = bytearray()
+        out += struct.pack('!II', self.num_bits, n0inv)
+        out += encode_long(self.num_bits, self.modulus)
+        out += encode_long(self.num_bits, rrmodn)
+        return bytes(out)
+
+def extract_public_key(in_key, out_file, password=None):
+    """Extract and write the encoded public key to file."""
+    pubkey = RSAPublicKey(in_key, password)
+    with open(out_file, 'wb') as f:
+        f.write(pubkey.encode())
+
+def main():
+    parser = argparse.ArgumentParser(description="Extract AVB public key.")
+    parser.add_argument('--key', required=True, help='Path to private key PEM')
+    parser.add_argument('--output', required=True, help='Output file')
+    parser.add_argument('--password', help='Optional key password (unsafe on command line)')
+
+    args = parser.parse_args()
+
+    password = args.password
+    if password is None:
+        try:
+            password = getpass.getpass("Enter key password (if any): ")
+        except KeyboardInterrupt:
+            print("\nCancelled.")
+            return
+
+    try:
+        extract_public_key(args.key, args.output, password if password else None)
+        print(f"Public key written to {args.output}")
+    except AvbError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+if __name__ == '__main__':
+    main()
--- generate_keys.sh
+++ generate_keys.sh
@@ -1,72 +1,92 @@
 #!/bin/bash
 
-# Set default values
 apex=false
 no_pass=false
+avb=false
 
-# Allow decrypted certs and optional apex certs
+# Parse CLI options
 while getopts ":hanv" opt; do
   case ${opt} in
-  h)
-    echo "Usage: generate_keys.sh [-a] [-n] [-v] [-h]"
-    echo "  -a   Generate apex certs"
-    echo "  -n   Do not prompt for password"
-    echo "  -v   Generate AVB certificate"
-    echo "  -h   Display this help message"
-    exit 0
-    ;;
-  a)
-    apex=true
-    ;;
-  n)
-    no_pass=true
-    ;;
-  v)
-    avb=true
-    ;;
-  \?)
-    echo "Invalid option: $OPTARG" 1>&2
-    exit 1
-    ;;
-  :)
-    echo "Invalid option: $OPTARG requires an argument" 1>&2
-    exit 1
-    ;;
+    h)
+      echo "Usage: $0 [-a] [-n] [-v] [-h]"
+      echo "  -a   Generate APEX certs"
+      echo "  -n   Do not prompt for password"
+      echo "  -v   Generate AVB certificate"
+      echo "  -h   Show this help message"
+      exit 0
+      ;;
+    a) apex=true ;;
+    n) no_pass=true ;;
+    v) avb=true ;;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
   esac
 done
 
-# Get the directory path of the script file and store it in the variable "dir"
 dir="$(dirname "$(realpath "$0")")"
 
-# Set the subject for the certificates
 subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=LineageOS/emailAddress=android@android.com'
 
-# Specify the certs to generate
-common=$(cat "$dir/common.list")
-
-# Optionally generate apex
-if [ "$apex" = true ]; then
-  common=$(cat "$dir/apex.list")
-fi
-
-if [ "$avb" = true ]; then
-  if [ "$no_pass" = true ]; then
-    openssl genrsa -out avb.pem 4096
-  else
-    openssl genrsa 4096 | openssl pkcs8 -topk8 -scrypt -out avb.pem
-  fi
-  python3 avbtool.py extract_public_key --key avb.pem --output avb_pkmd.bin
+if $apex; then
+  cert_list=$(<"$dir/apex.list")
 else
-  # Loop through the certificate names and generate keys using the make_key.sh script
-  echo "Note: Use same password for all certificates you generate"
-  for cert in $common; do
-    no_password=$no_pass "$dir"/make_key.sh "$cert" "$subject"
-    if [ "$apex" = true ]; then
-      if [ "$no_pass" = true ]; then
-        openssl pkcs8 -in "$cert.pk8" -inform DER -out "$cert.pem" -nocrypt
-      else
-        openssl pkcs8 -in "$cert.pk8" -inform DER -out "$cert.pem"
-      fi
-    fi
-  done
+  cert_list=$(<"$dir/common.list")
 fi
+
+if [ "$no_pass" != true ]; then
+  read -rsp "Enter password for certificate keys (leave blank for no password): " password
+  echo
+else
+  password=""
+fi
+readonly password
+
+generate_cert() {
+  local name="$1"
+  local keysize=4096
+  [[ "$name" == "verity" ]] && keysize=2048
+
+  if [[ -e "$name.pk8" || -e "$name.x509.pem" ]]; then
+    read -rp "Certificate '$name' exists. Overwrite? [y/N]: " reply
+    [[ "$reply" =~ ^[Yy]$ ]] || { echo "Skipping $name."; return; }
+  fi
+
+  echo "Generating cert: $name ($keysize-bit RSA)"
+
+  openssl genrsa -out "$name.key" "$keysize"
+
+  openssl req -new -x509 -sha256 -key "$name.key" -out "$name.x509.pem" \
+    -days 10000 -subj "$subject"
+
+  if [[ -z "$password" ]]; then
+    openssl pkcs8 -in "$name.key" -topk8 -outform DER -out "$name.pk8" -nocrypt
+  else
+    openssl pkcs8 -in "$name.key" -topk8 -v1 PBE-SHA1-3DES -outform DER \
+      -out "$name.pk8" -passout pass:"$password"
+  fi
+
+  if $apex; then
+    if [[ -z "$password" ]]; then
+      openssl pkcs8 -in "$name.pk8" -inform DER -out "$name.pem" -nocrypt
+    else
+      openssl pkcs8 -in "$name.pk8" -inform DER -out "$name.pem" -passin pass:"$password"
+    fi
+  fi
+
+  rm -f "$name.key"
+}
+
+if $avb; then
+  echo "Generating AVB key..."
+  if [[ -z "$password" ]]; then
+    openssl genrsa -out avb.pem 4096
+    python3 $dir/extract_public_key.py --key avb.pem --output avb_pkmd.bin
+  else
+    openssl genrsa 4096 | openssl pkcs8 -topk8 -scrypt -out avb.pem -passout pass:"$password"
+    python3 $dir/extract_public_key.py --key avb.pem --output avb_pkmd.bin --password "$password"
+  fi
+  exit 0
+fi
+
+for cert in $cert_list; do
+  generate_cert "$cert"
+done
